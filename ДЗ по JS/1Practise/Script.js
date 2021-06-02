b201fbdecb9c1ad37d7a9bbcfa76d18ad76445c9
SynyshynMax <synyshyn.maxim@gmail.com>
2021-06-02 19:23:36 +0300

    Vsi dz

---
+++ ДЗ по JS/1Practise/Script.js
@@ -0,0 +1,46 @@
+//.....//
+//.....//
+//first//
+//.....//
+//.....//
+let str = 'Привіт';
+
+console.log(typeof str);
+
+const num = 123;
+
+console.log(typeof  num);
+
+let flag = true;
+
+console.log(typeof flag);
+
+const txt = 'true';
+
+console.log(typeof txt);
+
+//
+//
+//second
+//
+//
+
+let guest = null; //null
+
+let name; //undefined
+
+const nameString = 'string'; //string
+
+const number = 9078561234;  //number
+
+let crimeaBoolean = true;  //boolean
+
+const nameCrimea = 'Ukraine'; //string
+
+let ourPhone = 380662540602; //nubmer
+
+let war; //undefinded
+
+const freeBuy = null; //null
+
+const ukraineBest = true;
